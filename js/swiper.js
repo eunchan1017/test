4e5dfed4489d79f957b6feaf4b16b5f5dc217fb2
eunchan1017 <jeongeunchan1017@gmail.com>
2024-10-23 16:19:03 +0900

    마크업 끝

--- js/swiper.js
+++ js/swiper.js
@@ -11,4 +11,18 @@ const featureSlider = new Swiper(".feature-slider", {
         nextEl: "#feature .btn-next",
         prevEl: "#feature .btn-prev",
     },
+    on: {
+        init: function () {
+            updateBackground(this);
+        },
+        slideChange: function () {
+            updateBackground(this);
+        },
+    },
 });
+
+function updateBackground(swiper) {
+    const activeSlide = swiper.slides[swiper.activeIndex];
+    const backgroundImage = activeSlide.dataset.background;
+    document.querySelector("#feature .background").style.backgroundImage = `url(${backgroundImage})`;
+}
