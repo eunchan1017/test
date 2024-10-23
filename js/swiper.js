4f32b6b25f5a872eef3ee9f4f71203e272129527
eunchan1017 <jeongeunchan1017@gmail.com>
2024-10-23 11:52:08 +0900

    feature 섹션 마크업 완료

--- js/swiper.js
+++ js/swiper.js
@@ -1,17 +1,16 @@
-const swiper = new Swiper(".swiper", {
-    // Optional parameters
-    direction: "vertical",
+const featureSlider = new Swiper(".feature-slider", {
     loop: true,
-
+    slidesPerView: 3,
+    spaceBetween: 40,
     // If we need pagination
     pagination: {
-        el: ".swiper-pagination",
+        el: "#feature .pagination",
     },
 
     // Navigation arrows
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: "#feature .btn-next",
+        prevEl: "#feature .btn-prev",
     },
 
     // And if we need scrollbar
